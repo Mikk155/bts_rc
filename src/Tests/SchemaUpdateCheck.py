f7593365ab145440166a8c2b2d05637a15b14320
Mikk155 <martimsanchez155@gmail.com>
2026-07-09 15:23:02 -0300

    Relocate all website assets

--- src/Tests/SchemaUpdateCheck.py
+++ src/Tests/SchemaUpdateCheck.py
@@ -26,7 +26,7 @@ class SchemaUpdateCheck( PyBuilder ):
 
             ShouldUpdate = False;
 
-            oldSchemaPath = Path( os.path.join( self.Workspace, "docs", "schema.json" ) );
+            oldSchemaPath = Path( os.path.join( self.Workspace, "docs", "page", "schema.json" ) );
             if os.path.exists( oldSchemaPath ):
                 with open( oldSchemaPath, "r", encoding="utf-8" ) as fOldSchema:
                     with open( path, "r", encoding="utf-8" ) as fNewSchema:
@@ -39,7 +39,7 @@ class SchemaUpdateCheck( PyBuilder ):
 
                 shutil.copyfile( path, oldSchemaPath );
                 self.Log( f"Updated documentation's schema. make sure to commit the change." );
-                return False;
+                return True;
 
         return True;
 
